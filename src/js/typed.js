e31f2d7437576a4da2fea809bc9276b2fd7dc36b
Swapneswarnayak <1705835@kiit.ac.in>
2022-12-11 21:24:50 +0530

    portfolio

---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["I'm a Full-Stack Developer", 'I Build  Websites', 'I Make Ideas into Projects'],
+  typeSpeed: 160,
+  backSpeed: 50,
+  backDelay: 1500
+  // loop: true
+};
+
+var typed = new Typed('#headline', options);
